c6af3b2cbfcf262832b252146adcbf2ce2f59178
ravenusmc <mcuddy77@gmail.com>
2017-09-19 19:55:39 -0400

    getting set up with Dash

---
+++ index.py
@@ -0,0 +1,29 @@
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+from apps import app1
+
+
+app.layout = html.Div([
+    html.H6('App 1'),
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        return app1.layout
+    # if pathname == '/apps/app1':
+    #     return app1.layout
+    # elif pathname == '/apps/app2':
+    #      return app2.layout
+    else:
+        return '404'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
